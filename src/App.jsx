3d627e6b6f4eaa2c320aadd87bbebb00424ac0ac
reagan luyinda <luyindareagan@gmail.com>
2026-03-07 18:18:07 +0300

    5.1 Blog List Frontend, step 1 ; create user login

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
+import Notification from './components/Notification'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,9 +17,49 @@ const App = () => {
     )  
   }, [])
 
+//login handler
+  const handleLogin = async event => {
+    event.preventDefault();
+    console.log("logging in with", username, password)
+
+    try {
+      const user = await loginService.login({username, password})
+      setUser(user);
+      setUsername("");
+      setPassword("");
+    } catch {
+      setErrorMessage("Wrong credentials");
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
+    }
+  }
+
+
+  if(user === null) {
+    return (
+    <div>
+        <h2>log in to application</h2>
+        <form onSubmit={handleLogin}>
+          <div>
+            <label>username <input type='text' value={username} onChange={({ target }) => setUsername(target.value)} /></label>
+          </div>
+          <div>
+            <label>password <input type="password" value={password} onChange={({ target }) => setPassword(target.value)} /></label>
+          </div>
+          <button type="submit">login</button>
+        </form>
+    </div>
+  )}
+
+  
   return (
     <div>
+       <Notification message={errorMessage} />
       <h2>blogs</h2>
+      {user && (<div>
+        <h3>{user.name} logged in</h3>
+      </div>)}
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
