0e5a4d7f98523dc8be22af01d26570bf2cf48436
reagan luyinda <luyindareagan@gmail.com>
2026-05-03 16:06:57 +0300

    5.14 Blog List tests, step 2; test button click to view blog url and likes

--- src/components/Blog.test.jsx
+++ src/components/Blog.test.jsx
@@ -1,25 +1,43 @@
 import { render, screen } from '@testing-library/react'
 import Blog from './Blog'
-import { test } from 'vitest'
+import userEvent from '@testing-library/user-event'
 
 describe('<Blog />', () => {
 
-  test('shows title and author, but not url or likes by default', () => {
-    const blog = {
-      title: 'vappu celebration',
-      author: 'yle news',
-      url: 'http://testblog.com',
-      likes: 5,
-      user: {
-        username: 'testuser',
-        name: 'Test User'
-      }
-
+  const blog = {
+    title: 'vappu celebration',
+    author: 'yle news',
+    url: 'http://testblog.com',
+    likes: 5,
+    user: {
+      username: 'testuser',
+      name: 'Test User'
     }
+
+  }
+
+  test('shows title and author, but not url or likes by default', () => {
     render(<Blog blog={blog} />)
 
     const titleElement = screen.getByText('vappu celebration yle news')
 
-    screen.debug(titleElement)
     expect(titleElement).toBeDefined()})
+
+
+  test('shows url and likes when view button is clicked', async () => {
+
+    render(<Blog blog={blog} />)
+
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+
+    const urlElement = screen.findByText('http://testblog.com')
+    const likesElement = screen.findByText('5')
+
+    screen.debug()
+
+    expect(urlElement).toBeDefined()
+    expect(likesElement).toBeDefined()
+  })
 })
